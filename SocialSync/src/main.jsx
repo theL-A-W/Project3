afdd038223a7d5576f94f99fdc9b3375814c422f
SimealWoldu <swoldu01@gmail.com>
2023-11-03 16:43:31 -0400

    simeal

--- SocialSync/src/main.jsx
+++ SocialSync/src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth0-react'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter>
+    <Auth0Provider 
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      redirectUri={import.meta.env.VITE_AUTH0_CALLBACK_URL}
+    >   
     <App />
+    </Auth0Provider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
