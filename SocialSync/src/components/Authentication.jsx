035030dd394cef3533d91a3c5f4617c68684694d
SimealWoldu <swoldu01@gmail.com>
2023-11-03 16:58:47 -0400

    simeal

--- SocialSync/src/components/Authentication.jsx
+++ SocialSync/src/components/Authentication.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import LoginButton from './LoginButton';
 import LogoutButton from './LogoutButton';
-
+j
 export default function Authentication() {
     const { isAuthenticated } = useAuth0();
 
