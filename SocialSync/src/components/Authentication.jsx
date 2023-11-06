61bba5c7ee86a8ed4f249001e6dc38b4fd5becfc
Lindsay Walker <lawalker98@gmail.com>
2023-11-06 16:43:45 -0500

    calendar added

--- SocialSync/src/components/Authentication.jsx
+++ SocialSync/src/components/Authentication.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import LoginButton from './LoginButton';
 import LogoutButton from './LogoutButton';
-j
+
 export default function Authentication() {
     const { isAuthenticated } = useAuth0();
 
     return (
         <div className="authentication">
-            <h3 id="sign-up">Sign-up</h3>
-            <h1 id="authentication-title">This is the authentication title</h1>
             {isAuthenticated ? <LogoutButton /> : <LoginButton />}
         </div>
     );
