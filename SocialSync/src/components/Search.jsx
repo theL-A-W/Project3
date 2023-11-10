d9dabd0bac4783ea667a565147a5f01361b2760b
TechALynch <Anthony.Lynch001@outlook.com>
2023-11-10 12:17:30 -0500

    Search.jsx Updates

--- SocialSync/src/components/Search.jsx
+++ SocialSync/src/components/Search.jsx
@@ -6,20 +6,21 @@ import DataContext from '../DataContext';
 export default function Search() {
   const navigate = useNavigate();
   const { eventDetailData, searchResultsData, setSearchResultsData, searchDisplay, setSearchDisplay } = useContext(DataContext);
-  const [searchName, setSearchName] = useState(''); // Add searchName state
+  const [searchName, setSearchName] = useState('');
+  const [formSubmitted, setFormSubmitted] = useState(false);
 
   const handleNameSubmit = async (e) => {
     e.preventDefault();
     if (searchName) {
       setSearchDisplay(searchName);
-      GetEventsByName(searchName); // Update to call GetEventsByName function
+      GetEventsByName(searchName);
+      setFormSubmitted(true); // Set formSubmitted to true on submit
     }
   }
 
-  // Function to search for events by name
   const GetEventsByName = async (name) => {
     try {
-      const response = await axios.get(`http://localhost:3001/events?name=${name}`); // Adjust the API endpoint URL
+      const response = await axios.get(`http://localhost:3001/events`);
       setSearchResultsData(response.data);
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -41,22 +42,24 @@ export default function Search() {
         </button>
       </form>
 
-      <div className="search-results-container">
-        {searchResultsData && searchResultsData.length > 0 ? (
-          <div className='search-results'>
-            <div className='search-results-query'>Showing results for "{searchDisplay}"</div>
-            <div className='search-results-grid'>
-              {searchResultsData.map((event, index) => (
-                <div className='search-results-grid-item' key={event._id} onClick={() => eventDetailsByID(event._id)}>
-                  <h2 className='event-title'>{event.title}</h2>
-                  <p className='event-description'>{event.description}</p>
-                  <p className='event-start-date'>Start Date: {event.startDate}</p>
-                </div>
-              ))}
+      {formSubmitted && ( // Render only if formSubmitted is true
+        <div className="search-results-container">
+          {searchResultsData && searchResultsData.length > 0 ? (
+            <div className='search-results'>
+              <div className='search-results-query'>Showing results for "{searchDisplay}"</div>
+              <div className='search-results-grid'>
+                {searchResultsData.map((event, index) => (
+                  <div className='search-results-grid-item' key={event._id} onClick={() => eventDetailsByID(event._id)}>
+                    <h2 className='event-title'>{event.title}</h2>
+                    <p className='event-description'>{event.description}</p>
+                    <p className='event-start-date'>Start Date: {event.startDate}</p>
+                  </div>
+                ))}
+              </div>
             </div>
-          </div>
-        ) : null}
-      </div>
+          ) : null}
+        </div>
+      )}
     </div>
   );
 }
